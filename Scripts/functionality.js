fe00dbbd1f08f0bcefc4fb6c6b22dac6f3cfe183
Anand Doshi <anand.doshi.8@gmail.com>
2020-09-21 11:49:29 +0530

    Implemented sidebar collapse button.

--- Scripts/functionality.js
+++ Scripts/functionality.js
@@ -1,9 +1,27 @@
-$(document).ready(function(){
-  $(document).scroll(function(){
+$(document).ready(function() {
+  sideBarScroller($("#sidebar"));//sidebar
+  sideBarScroller($(".btn.flush"));//sidear button
+  sideBarToggler();
+});
+
+var sideBarToggler = function() {
+  //var btn = document.getElementsByName("SideBarButton")[0];
+  var btn = $(".btn.flush")
+  //console.log(btn);
+  var sidebar = $("#sidebar");
+  var side = $(".side");
+  btn.click(function() {
+    sidebar.toggle();
+    side.toggle();
+    //console.log("toggle");
+  });
+};
+
+var sideBarScroller = function(sidebar) {
+  $(document).scroll(function() {
     var doc = $(document);
     var win = $(window);
 
-    var sidebar = $("#sidebar");
     var nav = $(".navbar");
     var top = $(".top");
     var topHeight = top.outerHeight();
@@ -13,20 +31,17 @@ $(document).ready(function(){
     var scroll = doc.scrollTop();
 
     //console.log(scroll+" "+ topHeight);
-    if(scroll > topHeight)
-    {
+    if (scroll > topHeight) {
       sidebar.addClass("fixed");
       sidebar.removeClass("relative");
-      sidebar.css("top", (navHeight)+"px");
-      console.log("fix");
+      sidebar.css("top", (navHeight) + "px");
+      //console.log("fix");
     }
-    if(scroll <= topHeight)
-    {
+    if (scroll <= topHeight) {
       sidebar.addClass("relative");
       sidebar.removeClass("fixed");
       sidebar.css("top", "inherit");
-      console.log("rel");
+      //console.log("rel");
     }
   });
-
-});
+};
