d955a2fa702639f6a8e9c063a1d63944c0f51569
Anand Doshi <anand.doshi.8@gmail.com>
2020-09-27 21:39:15 +0530

    Connected to db4free.net successfully! Databse should be up.

--- Scripts/connect_db.js
+++ Scripts/connect_db.js
@@ -1,19 +1,11 @@
 var mysql = require('mysql');
 
-// var con = mysql.createConnection({
-//   host: "sql201.epizy.com",
-//   user: "",//epiz_26833263
-//   password: "",//asNmTTXQRRaGyO
-//   database: "",//epiz_26833263_Blog
-//   port: ""//3306
-// });
-
 var con = mysql.createConnection({
-  host: "ensembldb.ensembl.org",
-  user: "",//epiz_26833263
-  password: "",//asNmTTXQRRaGyO
-  database: "",//epiz_26833263_Blog
-  port: ""//3306
+  host: "db4free.net",
+  user: "anand_doshi",
+  password: "SD8CgHefJD$zuu8",
+  database: "anand_doshi_blog",
+  port: "3306"
 });
 
   con.connect(function(err){
