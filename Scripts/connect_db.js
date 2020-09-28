da431041d1f6e43615437a3bfd61bdc44154394d
Anand Doshi <anand.doshi.8@gmail.com>
2020-09-28 11:00:08 +0530

    Added fetching functionality.

--- Scripts/connect_db.js
+++ Scripts/connect_db.js
@@ -1,5 +1,7 @@
 var mysql = require('mysql');
 
+var table_name = "Blog_Posts";
+
 var con = mysql.createConnection({
   host: "db4free.net",
   user: "anand_doshi",
@@ -12,5 +14,19 @@ var con = mysql.createConnection({
     if(err) throw err;
     else {
       console.log("Connected");
+      fetch();
     }
   });
+
+var fetch = function() {
+  con.query("SELECT * FROM "+table_name, function(err, result) {
+    if(err) throw err;
+    else {
+      for(var i=0; i<result.length; i++)
+      {
+        var row = result[i];
+        console.log(row);
+      }
+    }
+  });
+};
