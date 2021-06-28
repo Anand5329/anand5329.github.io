8419e28c804f92901cd580d0db0d9c1d052f16f2
Anand Doshi <anand.doshi.8@gmail.com>
2021-06-28 09:17:11 +0530

    Removed redundant files: blog, test.

--- Scripts/node_test.js
+++
@@ -1,8 +0,0 @@
-var request = require('request');
-
-request.get("https://app.infinityfree.net/accounts/epiz_26833263", {json: true}, function(err, res, body) {
-  if(err) throw err;
-  else {
-    console.log("Connected");
-  }
-})
